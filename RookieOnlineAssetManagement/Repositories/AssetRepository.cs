a4ff17df17735390c4b86977305b9aeb3c7bffe2
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-05-10 17:25:21 +0700

    Add AssetRepository

---
+++ RookieOnlineAssetManagement/Repositories/AssetRepository.cs
@@ -0,0 +1,48 @@
+﻿using RookieOnlineAssetManagement.Enums;
+using RookieOnlineAssetManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieOnlineAssetManagement.Repositories
+{
+    public class AssetRepository : IAssetRepository
+    {
+        public ICollection<AssetModel> GetListAsset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AssetModel> CreateAsset(AssetRequestModel assetRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AssetModel> DeleteAsset(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AssetModel> GetAsstById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+        public Task<AssetModel> UpdateAsset(string id, AssetRequestModel assetRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<AssetModel> GetListAsset(StateAsset state, string category, string query, SortBy sortCode, SortBy sortName, int sortCate, SortBy sortState)
+        {
+            var data = new List<AssetModel>();
+            var result = new List<AssetModel>();
+
+            if()
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RookieOnlineAssetManagement/Repositories/IAssetRepository.cs
@@ -0,0 +1,25 @@
+﻿using RookieOnlineAssetManagement.Enums;
+using RookieOnlineAssetManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieOnlineAssetManagement.Repositories
+{
+    public interface IAssetRepository
+    {
+        ICollection<AssetModel> GetListAsset(StateAsset state, string category, string query, SortBy? sortCode, SortBy? sortName, SortBy? sortCate, SortBy? sortState);
+
+        Task<AssetModel> GetAsstById(string id);
+
+        Task<AssetModel> CreateAsset(AssetRequestModel assetRequest);
+
+        Task<AssetModel> UpdateAsset(string id, AssetRequestModel assetRequest);
+
+        Task<AssetModel> DeleteAsset(string id);
+
+
+
+    }
+}
