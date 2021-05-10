931b1471b68612a7cafb1c9213d8e5c2961a7105
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-05-10 17:25:46 +0700

    Update View Model

--- RookieOnlineAssetManagement/Models/UserModel.cs
+++ RookieOnlineAssetManagement/Models/UserModel.cs
@@ -1,10 +1,14 @@
-﻿namespace RookieOnlineAssetManagement.Models
+﻿using System;
+
+namespace RookieOnlineAssetManagement.Models
 {
     public class UserModel
     {
         public string Id { get; set; }
         public string UserName { get; set; }
+        public string FullName { get; set; }
         public string Email { get; set; }
         public string RoleName { get; set; }
+        public DateTime? JoinedDate { get; set; }
     }
 }
