79be203cf3b4d64c65b246a6a2c2f849d5e9e466
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-05-11 18:13:35 +0700

    Update User Repository Class

--- RookieOnlineAssetManagement/Repositories/IUserRepository.cs
+++ RookieOnlineAssetManagement/Repositories/IUserRepository.cs
@@ -1,12 +1,13 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using RookieOnlineAssetManagement.Enums;
 using RookieOnlineAssetManagement.Models;
 
 namespace RookieOnlineAssetManagement.Repositories
 {
     public interface IUserRepository
     {
-        Task<ICollection<UserModel>> GetListAsync();
+        Task<ICollection<UserModel>> GetListUserAsync(string locationId, TypeUser type, string query, SortBy? sortCode, SortBy? sortName, SortBy? sortDate, SortBy? sortType, int page, int pageSize);
 
         Task<UserModel> GetUserByIdAsync(string id);
 
--- RookieOnlineAssetManagement/Repositories/UserRepository.cs
+++ RookieOnlineAssetManagement/Repositories/UserRepository.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using RookieOnlineAssetManagement.Data;
+using RookieOnlineAssetManagement.Enums;
 using RookieOnlineAssetManagement.Models;
 
 namespace RookieOnlineAssetManagement.Repositories
@@ -16,11 +17,81 @@ namespace RookieOnlineAssetManagement.Repositories
             _dbContext = dbContext;
         }
 
-        public async Task<ICollection<UserModel>> GetListAsync()
+        public async Task<ICollection<UserModel>> GetListUserAsync(string locationId, TypeUser? type, string query, SortBy? sortCode, SortBy? sortFullName, SortBy? sortDate, SortBy? sortType, int page, int pageSize)
         {
-            return await _dbContext.Users
-                .Select(item => new UserModel { Id = item.Id, UserName = item.UserName, Email = item.Email })
-                .ToListAsync();
+            var queryable = _dbContext.Users.Where(item => item.LocationId == locationId);
+            if (!string.IsNullOrEmpty(query))
+            {
+                queryable = queryable.Where(x => x.Id.Contains(query) || x.UserName.Contains(query));
+            }
+            if (sortCode != null)
+                switch (sortCode)
+                {
+                    case SortBy.ASC:
+                        queryable = queryable.OrderBy(item => item.StaffCode);
+                        break;
+                    case SortBy.DESC:
+                        queryable = queryable.OrderByDescending(item => item.StaffCode);
+                        break;
+                }
+            if (sortFullName.HasValue)
+            {
+                switch (sortFullName)
+                {
+                    case SortBy.ASC:
+                        queryable = queryable.OrderBy(item => item.FirstName).OrderBy(item=>item.LastName);
+                        break;
+                    case SortBy.DESC:
+                        queryable = queryable.OrderByDescending(item => item.FirstName).OrderBy(item => item.LastName);
+                        break;
+                }              
+            }
+
+            if (sortDate.HasValue)
+            {
+                switch (sortDate)
+                {
+                    case SortBy.ASC:
+                        queryable = queryable.OrderBy(item => item.JoinedDate);
+                        break;
+                    case SortBy.DESC:
+                        queryable = queryable.OrderByDescending(item => item.JoinedDate);
+                        break;
+                }
+            }
+
+            
+            queryable
+             .Join(
+                      _dbContext.UserRoles,
+                      user => user.Id,
+                      userRole => userRole.UserId,
+                      (user, userrole) => new
+                      {
+                          UserId = user.Id,
+                          RoleId = userrole.RoleId,
+                          StaffCode = user.StaffCode,
+                          FullName = $"{user.FirstName} {user.LastName}",
+                          UserName = user.UserName,
+                          JoinedDate = user.JoinedDate
+                      }
+                  )
+             .Join(
+                       _dbContext.Roles,
+                       userRole => userRole.RoleId,
+                       role => role.Id,
+                       (userRole, role) => new
+                       {
+                           RoleName = role.Name,
+
+                       }
+                  );
+
+            //if (type.HasValue)
+            //{
+            //    queryable = queryable.Where(x => x.);
+            //}
+            
         }
 
         public Task<UserModel> GetUserByIdAsync(string id)
