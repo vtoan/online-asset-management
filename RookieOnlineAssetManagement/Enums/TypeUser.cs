186cdbe0e43dde34e3ee47838a64463254821d90
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-05-11 18:12:09 +0700

    Update Enums

---
+++ RookieOnlineAssetManagement/Enums/TypeUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieOnlineAssetManagement.Enums
+{
+    public enum TypeUser
+    {
+        Admin = 1, Staff = 2,leduyen=3
+    }
+}
