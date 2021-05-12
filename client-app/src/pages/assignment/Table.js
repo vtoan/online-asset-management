7f6cb0523d434b84521f47b3ec23baba2d41775f
Mike0704 <haiduong7499@gmail.com>
2021-05-12 10:07:14 +0700

    modified table assignment

--- client-app/src/pages/assignment/Table.js
+++ client-app/src/pages/assignment/Table.js
@@ -1,4 +1,4 @@
-import { Col, Row, Button } from 'reactstrap';
+import { Table, Button } from 'reactstrap';
 import '../../index.css';
 import { BsPencil } from "react-icons/bs";
 import TableItem from '../../common/TableItem';
@@ -11,59 +11,49 @@ export default function AssignmentTable() {
         console.log("onChanged");
     }
     return (
-        <div>
-            <Row style={{ "fontSize": "12px", "fontWeight": "500" }}>
-                <Col> <TableItem>  <LableItems title="No" onChanged={onChangeSortId}></LableItems></TableItem>  </Col>
-                <Col xs={1}> <TableItem>  <LableItems title="Code" onChanged={onChangeSortId}></LableItems> </TableItem>  </Col>
-                <Col xs={2}> <TableItem>  <LableItems title="Asset name" onChanged={onChangeSortId}></LableItems></TableItem>  </Col>
-                <Col xs={2}> <TableItem>  <LableItems title="Assigned to" onChanged={onChangeSortId}></LableItems></TableItem>  </Col>
-                <Col xs={2}> <TableItem>  <LableItems title="Assigned by" onChanged={onChangeSortId}></LableItems></TableItem>  </Col>
-                <Col xs={2}> <TableItem>  <LableItems title="Assigned date" onChanged={onChangeSortId}></LableItems></TableItem>  </Col>
-                <Col xs={1}> <TableItem>  <LableItems title="Status" onChanged={onChangeSortId}></LableItems></TableItem>  </Col>
-                <Col> </Col>
-            </Row>
-            <Row style={{ "fontSize": "14px" }}>
-                <Col>
-                    <p>1</p>
-                </Col>
+        <Table borderless>
+            <thead>
+                <tr style={{ "fontSize": "14px", "fontWeight": "500" }}>
+                    <th> <TableItem>  <LableItems title="No" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
+                    <th> <TableItem>  <LableItems title="Code" onChanged={onChangeSortId}></LableItems> </TableItem>  </th>
+                    <th> <TableItem>  <LableItems title="Asset name" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
+                    <th> <TableItem>  <LableItems title="Assigned to" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
+                    <th> <TableItem>  <LableItems title="Assigned by" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
+                    <th> <TableItem>  <LableItems title="Assigned date" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
+                    <th> <TableItem>  <LableItems title="Status" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
+                    <th> </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
 
-                <Col xs={1}>
-                    <p>MO100005</p>
-                </Col>
+                    <td>MO100005</td>
 
-                <Col xs={2}>
-                    <p>Monitor Dell UltraSharp</p>
-                </Col>
+                    <td>Monitor Dell UltraSharp</td>
 
-                <Col xs={2}>
-                    <p>dhnd</p>
-                </Col>
+                    <td>dhnd</td>
 
-                <Col xs={2}>
-                    <p>dvt</p>
-                </Col>
+                    <td>dvt</td>
 
-                <Col xs={2}>
-                    <p>8/5/2020</p>
-                </Col>
+                    <td>8/5/2020</td>
 
-                <Col xs={1}>
-                    <p>Accepted</p>
-                </Col>
+                    <td>Accepted</td>
 
-                <Col >
-                    <Button color="#fff" className="py-0 px-0" style={{ "lineHeight": "0px" }}>
-                        <BsPencil />
-                    </Button>
-                    <Button color="#fff" className="py-0 px-0" style={{ "lineHeight": "0px" }}>
-                        <TiDeleteOutline />
-                    </Button>
-                    <Button color="#fff" className="py-0 px-0" style={{ "lineHeight": "0px" }}>
-                        <TiRefresh />
-                    </Button>
+                    <td >
+                        <Button color="#fff" className="py-0 px-0" style={{ "lineHeight": "0px" }}>
+                            <BsPencil />
+                        </Button>
+                        <Button color="#fff" className="py-0 px-0" style={{ "lineHeight": "0px" }}>
+                            <TiDeleteOutline />
+                        </Button>
+                        <Button color="#fff" className="py-0 px-0" style={{ "lineHeight": "0px" }}>
+                            <TiRefresh />
+                        </Button>
 
-                </Col>
-            </Row>
-        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </Table>
     );
 }
