572ff5fcebbad8d81fb802c8965138ea8830d5f2
DangToan <dtoan2805@gmail.com>
2021-05-13 00:25:16 +0700

    Update LableItem Compoent

--- client-app/src/common/LableItems.js
+++ client-app/src/common/LableItems.js
@@ -1,18 +1,24 @@
-import "../index.css"
-import React from 'react';
-import {BsArrowDownShort } from "react-icons/bs";
+import React from "react";
+import { BsArrowDownShort } from "react-icons/bs";
 
-export default function LableItems({ title, onChanged }) { 
-    const [sortState, setSort] = React.useState("");
-    const handleClick = () => {
-        setSort(sortState !== "" ? "" : "rotate(180deg)");
-        onChanged();
-    }
-    return (
-        <div style={{"cursor":"pointer","fontWeight":700}} onClick={handleClick}>
-            <lable >{title}</lable>
-            <BsArrowDownShort  className="lable-arrow" style={{ "transform": sortState }} />
-        </div>
-    )
+export default function LableItems({ title, nameSort, reset, onChanged }) {
+  const [sortState, setSort] = React.useState("");
+  React.useEffect(() => {
+    if (nameSort !== reset) setSort("");
+  }, [reset, nameSort]);
+  const handleClick = () => {
+    setSort(sortState !== "" ? "" : "rotate(180deg)");
+    let target = {};
+    target[nameSort] = sortState === "" ? "desc" : "asc";
+    onChanged(target);
+  };
+  return (
+    <div style={{ cursor: "pointer", fontWeight: 700 }} onClick={handleClick}>
+      <span>{title}</span>
+      <BsArrowDownShort
+        className="lable-arrow"
+        style={{ transform: sortState, fontSize: "1.5em" }}
+      />
+    </div>
+  );
 }
-
