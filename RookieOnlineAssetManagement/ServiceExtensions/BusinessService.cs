72c7f5f4a09e2a06c7f89ce07b6b35c589abae6a
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-05-12 10:04:18 +0700

    edit BussinessService

--- RookieOnlineAssetManagement/ServiceExtensions/BusinessService.cs
+++ RookieOnlineAssetManagement/ServiceExtensions/BusinessService.cs
@@ -8,6 +8,7 @@ namespace RookieOnlineAssetManagement.ServiceExtensions
         public static IServiceCollection AddBusinessService(this IServiceCollection services)
         {
             services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IAssetService, AssetService>();
 
             return services;
         }
