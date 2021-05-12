7e0605b0aa424c99c0578a07ee629b84c2be7a84
Mike0704 <haiduong7499@gmail.com>
2021-05-12 10:22:59 +0700

    modified table asset

--- client-app/src/pages/asset/Table.js
+++ client-app/src/pages/asset/Table.js
@@ -1,8 +1,8 @@
-import { Col, Row, Button } from 'reactstrap';
+import { Table, Button } from 'reactstrap';
 import '../../index.css';
 import React from 'react';
 import { TiDeleteOutline } from "react-icons/ti";
-import { BsPencil, BsArrowDownShort } from "react-icons/bs";
+import { BsPencil } from "react-icons/bs";
 import LableItems from '../../common/LableItems';
 import TableItem from '../../common/TableItem'
 
@@ -14,7 +14,7 @@ import TableItem from '../../common/TableItem'
 export default function AssetTable() {
 
     const onChangeSortId = () => {
-        console.log("onChanged") 
+        console.log("onChanged")
         //call api  
     }
 
@@ -29,36 +29,39 @@ export default function AssetTable() {
             id: "HD1111", name: "Laptop asd ", category: "Laptop", status: 1,
         }
     ]
-
-
-
     return (
         <div>
-            <Row>
-                <Col xs={2}> <TableItem> <LableItems title="Asset ID" onChanged={onChangeSortId}/> </TableItem>  </Col>
-                <Col xs={4}> <TableItem> <LableItems title="Asset Name" onChanged={onChangeSortId}/>  </TableItem>  </Col>
-                <Col xs={2}> <TableItem> <LableItems title="Category" onChanged={onChangeSortId}/> </TableItem>  </Col>
-                <Col xs={2}> <TableItem> <LableItems title="Status" onChanged={onChangeSortId}/> </TableItem>  </Col>
-                <Col xs={2}> </Col>
-            </Row>
-            <Row>
-                {seedData.map(item => (
-                    <>
-                        <Col xs={2}> <TableItem> <p>{item.id}</p> </TableItem> </Col>
-                        <Col xs={4}> <TableItem> <p>{item.name}</p> </TableItem> </Col>
-                        <Col xs={2}> <TableItem> <p>{item.category}</p> </TableItem> </Col>
-                        <Col xs={2}> <TableItem> <p>Available</p> </TableItem> </Col>
-                        <Col xs={2}>
-                            <Button color="#fff" className="py-0">
-                                <BsPencil color="#0d6efd" />
-                            </Button>
-                            <Button color="#fff" className="py-0 border-0">
-                                <TiDeleteOutline color="#dc3545" />
-                            </Button>
-                        </Col>
-                    </>
-                ))}
-            </Row>
+            <Table borderless>
+                <thead>
+                    <tr>
+                        <td> <TableItem> <LableItems title="Asset ID" onChanged={onChangeSortId} /> </TableItem></td>
+                        <td> <TableItem> <LableItems title="Asset Name" onChanged={onChangeSortId} /> </TableItem></td>
+                        <td> <TableItem> <LableItems title="Category" onChanged={onChangeSortId} /></TableItem></td>
+                        <td> <TableItem> <LableItems title="Status" onChanged={onChangeSortId} /></TableItem></td>
+                        <td> </td>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {seedData.map(item => (
+                        <tr>
+                            <td> <TableItem> <p>{item.id}</p> </TableItem> </td>
+                            <td> <TableItem> <p>{item.name}</p> </TableItem> </td>
+                            <td> <TableItem> <p>{item.category}</p> </TableItem> </td>
+                            <td> <TableItem> <p>Available</p> </TableItem> </td>
+                            <td>
+                                <Button color="#fff" className="py-0">
+                                    <BsPencil color="#0d6efd" />
+                                </Button>
+                                <Button color="#fff" className="py-0 border-0">
+                                    <TiDeleteOutline color="#dc3545" />
+                                </Button>
+                            </td>
+                        </tr>
+                    ))}
+
+                </tbody>
+            </Table>
         </div>
     );
 }
