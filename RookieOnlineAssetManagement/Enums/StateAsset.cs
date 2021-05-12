4af72adb86c2adde1d7eb3c045e0bf5dc38f58cc
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-05-12 10:03:30 +0700

    format enum_StateAsset

--- RookieOnlineAssetManagement/Enums/StateAsset.cs
+++ RookieOnlineAssetManagement/Enums/StateAsset.cs
@@ -7,6 +7,6 @@ namespace RookieOnlineAssetManagement.Enums
 {
     public enum StateAsset
     {
-        Assigned =1, Avaiable =2, NotAvaiable=3, WatingRecycling=4, Recycled=5
+        Assigned = 1, Avaiable = 2, NotAvaiable = 3, WatingRecycling = 4, Recycled = 5
     }
 }
