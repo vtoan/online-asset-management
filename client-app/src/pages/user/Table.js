89a6444354cad7c0c296d1c5a079abc06edf5edf
Mike0704 <haiduong7499@gmail.com>
2021-05-13 15:43:08 +0700

    modified table user

--- client-app/src/pages/user/Table.js
+++ client-app/src/pages/user/Table.js
@@ -1,62 +1,74 @@
-import { Col, Row, Button, Table } from 'reactstrap';
-import '../../index.css';
 import TableItem from '../../common/TableItem';
-import LableItems from '../../common/LableItems'
+import NSTable from "../../common/NSTable";
 import { BsPencil } from "react-icons/bs";
 import { TiDeleteOutline } from "react-icons/ti";
 
+const tableTitles = [
+    {
+        title: "StaffCode",
+        nameSort: "sort StaffCode",
+    },
+    {
+        title: "FullName",
+        nameSort: "sort FullName",
+    },
+    {
+        title: "UserName",
+        nameSort: "sort UserName",
+    },
+    {
+        title: "JoinedDate",
+        nameSort: "sort JoinedDate",
+    },
+    {
+        title: "Type",
+        nameSort: "sort Type",
+    },
+]
 
-export default function UserTable() {
-    const onChangeSortId = () => {
-        console.log("onChanged")
-        //call api  
-    }
-
-    const seedData = [
-        {
-            code: "HD1111", fullName: "Laptop asd ", userName: "Laptop", joinedDate: "07/04/2021", Type: "Staff"
-        },
-        {
-            code: "HD1111", fullName: "Laptop asd ", userName: "Laptop", joinedDate: "20/5/2020", Type: "Staff"
-        },
-        {
-            code: "HD1111", fullName: "Laptop asd ", userName: "Laptop", joinedDate: "13/1/2019", Type: "Staff"
-        }
-    ]
-
+export default function UserTable({
+    datas,
+    totalPage,
+    onChangePage,
+    onChangeSort,
+    onEdit,
+    onDelete,
+}) {
+    const itemRender = (item) => (
+        <>
+            <td>
+                <TableItem>{item.code}</TableItem>
+            </td>
+            <td>
+                <TableItem>{item.fullName}</TableItem>
+            </td>
+            <td>
+                <TableItem>{item.userName}</TableItem>
+            </td>
+            <td>
+                <TableItem>{item.joinedDate}</TableItem>
+            </td>
+            <td>
+                <TableItem>{item.Type}</TableItem>
+            </td>
+            <td className="table-actions">
+                <span className="table-icon" onClick={() => onEdit && onEdit(item)}>
+                    <BsPencil color="#0d6efd" />
+                </span>
+                <span className="table-icon" onClick={() => onDelete && onDelete(item)}>
+                    <TiDeleteOutline color="#dc3545" />
+                </span>
+            </td>
+        </>
+    );
     return (
-        <div>
-            <Table borderless>
-                <thead>
-                    <tr style={{ "fontSize": "14px", "fontWeight": "500" }}>
-                        <td> <TableItem><LableItems title="StaffCode" onChanged={onChangeSortId} /></TableItem></td>
-                        <td> <TableItem><LableItems title="FullName" onChanged={onChangeSortId} /></TableItem></td>
-                        <td> <TableItem><LableItems title="UserName" onChanged={onChangeSortId} /></TableItem></td>
-                        <td> <TableItem><LableItems title="JoinedDate" onChanged={onChangeSortId} /></TableItem></td>
-                        <td> <TableItem><LableItems title="Type" onChanged={onChangeSortId} /></TableItem></td>
-                        <td> </td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {seedData.map(item => (
-                        <tr>
-                            <td> <TableItem> <p>{item.code}</p> </TableItem> </td>
-                            <td> <TableItem> <p>{item.fullName}</p> </TableItem> </td>
-                            <td> <TableItem> <p>{item.userName}</p> </TableItem> </td>
-                            <td> <TableItem> <p>{item.joinedDate}</p> </TableItem> </td>
-                            <td> <TableItem> <p>{item.Type}</p> </TableItem> </td>
-                            <td>
-                                <Button color="#fff" className="py-0">
-                                    <BsPencil color="#0d6efd" />
-                                </Button>
-                                <Button color="#fff" className="py-0 border-0">
-                                    <TiDeleteOutline color="#dc3545" />
-                                </Button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
-        </div>
+        <NSTable
+            titles={tableTitles}
+            datas={datas}
+            totalPages={totalPage}
+            itemRender={itemRender}
+            onChangeSort={onChangeSort}
+            onChangePage={onChangePage}
+        />
     );
 }
--- client-app/src/pages/user/User.js
+++ client-app/src/pages/user/User.js
@@ -1,8 +1,73 @@
 import React from "react";
 import UserTable from "./Table";
 
+const seedData = [
+  {
+    code: "HD1111",
+    fullName: "Laptop asd ",
+    userName: "Laptop",
+    joinedDate: "07/04/2021",
+    Type: "Staff"
+  },
+  {
+    code: "HD1111",
+    fullName: "Laptop asd ",
+    userName: "Laptop",
+    joinedDate: "07/04/2021",
+    Type: "Staff"
+  },
+  {
+    code: "HD1111",
+    fullName: "Laptop asd ",
+    userName: "Laptop",
+    joinedDate: "07/04/2021",
+    Type: "Staff"
+  }
+]
+
 export default function User() {
+  const [userDatas, setUser] = React.useState([]);
+  const [totalPages, setTotalPages] = React.useState(0);
+
+  React.useEffect(() => {
+    _fetchData();
+  }, []);
+
+  const _fetchData = () => {
+    setUser([]);
+    setTimeout(() => {
+      setUser(seedData);
+      setTotalPages(2);
+    }, 500)
+  }
+
+  //handleClick
+
+  const handleChangePage = (page) => {
+    console.log(page);
+    _fetchData();
+  };
+
+  const handleChangeSort = (target) => {
+    console.log(target);
+    _fetchData();
+  };
+
+  const handleEdit = (item) => {
+    console.log(item);
+  };
+
+  const handleDelete = (item) => {
+    console.log(item);
+  };
+
   return <div>
-    <UserTable/>
+    <UserTable
+      datas={userDatas}
+      onChangePage={handleChangePage}
+      onChangeSort={handleChangeSort}
+      onEdit={handleEdit}
+      onDelete={handleDelete}
+      totalPage={totalPages} />
   </div>;
 }
