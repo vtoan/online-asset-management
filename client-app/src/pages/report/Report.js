f0668e09a1b251efa1426ba8dc58e6824c7e962a
Mike0704 <haiduong7499@gmail.com>
2021-05-13 16:00:47 +0700

    modified table report

--- client-app/src/pages/report/Report.js
+++ client-app/src/pages/report/Report.js
@@ -1,5 +1,71 @@
 import React from "react";
 import ReportTable from './Table.js';
+
+const seedData = [
+  {
+    Category: "BluetoothMouse",
+    Total: "1000",
+    Assigned: "550",
+    Available: "450",
+    NotAvailable: "0",
+    Waitingforrecycling: "0",
+    Recycled: "0",
+
+  },
+  {
+    Category: "BluetoothMouse",
+    Total: "1000",
+    Assigned: "550",
+    Available: "450",
+    NotAvailable: "0",
+    Waitingforrecycling: "0",
+    Recycled: "0",
+  },
+  {
+    Category: "BluetoothMouse",
+    Total: "1000",
+    Assigned: "550",
+    Available: "450",
+    NotAvailable: "0",
+    Waitingforrecycling: "0",
+    Recycled: "0",
+  }
+]
 export default function Report() {
-  return <ReportTable></ReportTable>;
+  const [reportData, setReport] = React.useState([]);
+  const [totalPages, setTotalPages] = React.useState(0);
+
+  React.useEffect(() => {
+    _fetchData();
+  }, []);
+
+  const _fetchData = () => {
+    setReport([]);
+    setTimeout(() => {
+      setReport(seedData);
+      setTotalPages(2);
+    }, 500)
+  }
+
+  //handleClick
+  const handleChangePage = (page) => {
+    console.log(page);
+    _fetchData();
+  };
+
+  const handleChangeSort = (target) => {
+    console.log(target);
+    _fetchData();
+  };
+
+  return (
+    <div>
+      <ReportTable
+        datas={reportData}
+        onChangePage={handleChangePage}
+        onChangeSort={handleChangeSort}
+        totalPage={totalPages}
+      />
+    </div>
+  );
 }
--- client-app/src/pages/report/Table.js
+++ client-app/src/pages/report/Table.js
@@ -2,43 +2,77 @@ import { Table, Button } from 'reactstrap';
 import '../../index.css';
 import TableItem from '../../common/TableItem';
 import LableItems from '../../common/LableItems'
+import NSTable from '../../common/NSTable';
 
 
-
-export default function ReportTable() {
-    const onChangeSortId = () => {
-        console.log("onChanged");
-    }
-    return (
-        <Table borderless>
-            <thead>
-                <tr style={{ "fontSize": "14px", "fontWeight": "500" }}>
-                    <th> <TableItem>  <LableItems title="Category" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
-                    <th> <TableItem>  <LableItems title="Total" onChanged={onChangeSortId}></LableItems> </TableItem>  </th>
-                    <th> <TableItem>  <LableItems title="Assigned" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
-                    <th> <TableItem>  <LableItems title="Available" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
-                    <th> <TableItem>  <LableItems title="Not Available" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
-                    <th> <TableItem>  <LableItems title="Waiting for recycling" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
-                    <th> <TableItem>  <LableItems title="Recycled" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Bluetooth Mouse</td>
-
-                    <td>1000</td>
-
-                    <td>550</td>
-
-                    <td>450</td>
-
-                    <td>0</td>
-
-                    <td>0</td>
-
-                    <td>0</td>
-                </tr>
-            </tbody>
-        </Table>
+const tableTitles = [
+    {
+        title: "Category",
+        nameSort: "sort Category",
+    },
+    {
+        title: "Total",
+        nameSort: "sort Total",
+    },
+    {
+        title: "Assigned",
+        nameSort: "sort Assigned",
+    },
+    {
+        title: "Available",
+        nameSort: "sort Available",
+    },
+    {
+        title: "Not Available",
+        nameSort: "sort Not Available",
+    },
+    {
+        title: "Waiting for recycling",
+        nameSort: "sort Waiting for recycling",
+    },
+    {
+        title: "Recycled",
+        nameSort: "sort Recycled",
+    },
+]
+export default function ReportTable({
+    datas,
+    totalPage,
+    onChangePage,
+    onChangeSort,
+}) {
+    const itemRender = (item) => (
+        <>
+            <td>
+                <TableItem>{item.Category}</TableItem>
+            </td>
+            <td>
+                <TableItem>{item.Total}</TableItem>
+            </td>
+            <td>
+                <TableItem>{item.Assigned}</TableItem>
+            </td>
+            <td>
+                <TableItem>{item.Available}</TableItem>
+            </td>
+            <td>
+                <TableItem>{item.NotAvailable}</TableItem>
+            </td>
+            <td>
+                <TableItem>{item.Waitingforrecycling}</TableItem>
+            </td>
+            <td>
+                <TableItem>{item.Recycled}</TableItem>
+            </td>
+        </>
     );
+    return (
+        <NSTable
+            titles={tableTitles}
+            datas={datas}
+            totalPages={totalPage}
+            itemRender={itemRender}
+            onChangeSort={onChangeSort}
+            onChangePage={onChangePage} />
+    )
 }
