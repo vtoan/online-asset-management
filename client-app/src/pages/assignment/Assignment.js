2a37c00b96b3eac5ff58542ad6d6a9f4337553d8
Mike0704 <haiduong7499@gmail.com>
2021-05-13 15:08:24 +0700

    add event in Assignment table

--- client-app/src/pages/assignment/Assignment.js
+++ client-app/src/pages/assignment/Assignment.js
@@ -1,10 +1,86 @@
 import React from "react";
 import AssignmentTable from "./Table";
 
+const seedData = [
+  {
+    No: 1,
+    Code: "MO100005",
+    Assetname: "Monitor Dell UltraSharp",
+    Assignedto: "ndhd",
+    Assignedby: "dvt",
+    AssignedDate: "8/5/2020",
+    Status: "Accepted"
+
+  },
+  {
+    No: 2,
+    Code: "MO100005",
+    Assetname: "Monitor Dell UltraSharp",
+    Assignedto: "ndhd",
+    Assignedby: "dvt",
+    AssignedDate: "8/5/2020",
+    Status: "Accepted"
+
+  },
+  {
+    No: 3,
+    Code: "MO100005",
+    Assetname: "Monitor Dell UltraSharp",
+    Assignedto: "ndhd",
+    Assignedby: "dvt",
+    AssignedDate: "8/5/2020",
+    Status: "Accepted"
+
+  },
+]
 export default function Assignment() {
+
+  const [assignmentData, setAssignment] = React.useState([]);
+  const [totalPages, setTotalPages] = React.useState(0);
+
+  React.useEffect(() => {
+    _fetchData();
+  }, [])
+
+  const _fetchData = () => {
+    setAssignment([]);
+    setTimeout(() => {
+      setAssignment(seedData);
+      setTotalPages(2);
+    }, 500)
+  }
+  const onEdit = (item) => {
+    console.log(item);
+  }
+
+  const onRefresh = (item) => {
+    console.log(item);
+  }
+
+  const onDeny = (item) => {
+    console.log(item);
+  }
+  const onChangeSort = (target) => {
+    console.log(target);
+    _fetchData();
+  }
+  const onChangePage = (page) => {
+    console.log(page);
+    _fetchData();
+  }
+
   return (
-    <AssignmentTable>
-      
-    </AssignmentTable>
+    <>
+    <AssignmentTable
+      datas={assignmentData}
+      totalPage={totalPages}
+      onEdit={onEdit}
+      onDeny={onDeny}
+      onRefresh={onRefresh}
+      onChangeSort={onChangeSort}
+      onChangePage={onChangePage}
+    />
+    </>
+    
   );
 }
--- client-app/src/pages/assignment/Table.js
+++ client-app/src/pages/assignment/Table.js
@@ -1,59 +1,95 @@
-import { Table, Button } from 'reactstrap';
 import '../../index.css';
 import { BsPencil } from "react-icons/bs";
 import TableItem from '../../common/TableItem';
-import LableItems from '../../common/LableItems'
 import { TiDeleteOutline, TiRefresh } from "react-icons/ti";
+import NSTable from '../../common/NSTable';
 
 
-export default function AssignmentTable() {
-    const onChangeSortId = () => {
-        console.log("onChanged");
-    }
-    return (
-        <Table borderless>
-            <thead>
-                <tr style={{ "fontSize": "14px", "fontWeight": "500" }}>
-                    <th> <TableItem>  <LableItems title="No" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
-                    <th> <TableItem>  <LableItems title="Code" onChanged={onChangeSortId}></LableItems> </TableItem>  </th>
-                    <th> <TableItem>  <LableItems title="Asset name" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
-                    <th> <TableItem>  <LableItems title="Assigned to" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
-                    <th> <TableItem>  <LableItems title="Assigned by" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
-                    <th> <TableItem>  <LableItems title="Assigned date" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
-                    <th> <TableItem>  <LableItems title="Status" onChanged={onChangeSortId}></LableItems></TableItem>  </th>
-                    <th> </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
 
-                    <td>MO100005</td>
-
-                    <td>Monitor Dell UltraSharp</td>
-
-                    <td>dhnd</td>
-
-                    <td>dvt</td>
-
-                    <td>8/5/2020</td>
-
-                    <td>Accepted</td>
-
-                    <td >
-                        <Button color="#fff" className="py-0 px-0" style={{ "lineHeight": "0px" }}>
-                            <BsPencil />
-                        </Button>
-                        <Button color="#fff" className="py-0 px-0" style={{ "lineHeight": "0px" }}>
-                            <TiDeleteOutline />
-                        </Button>
-                        <Button color="#fff" className="py-0 px-0" style={{ "lineHeight": "0px" }}>
-                            <TiRefresh />
-                        </Button>
-
-                    </td>
-                </tr>
-            </tbody>
-        </Table>
+const tableTitles = [
+    {
+        title: 'No',
+        sortName: 'sortNumber'
+    },
+    {
+        title: 'Code',
+        sortName: 'sortCode'
+    },
+    {
+        title: 'Asset Name',
+        sortName: 'sort Asset Name'
+    },
+    {
+        title: 'Assigned to',
+        sortName: 'sort Assigned to'
+    },
+    {
+        title: 'Assignedby',
+        sortName: 'sort Assignedby'
+    },
+    {
+        title: 'Asset Assigned Date',
+        sortName: 'sort Assigned Date'
+    },
+    {
+        title: 'Asset Status',
+        sortName: 'sort Status'
+    },
+]
+export default function AssignmentTable({
+    datas,
+    totalPage,
+    onChangePage,
+    onChangeSort,
+    onEdit,
+    onDeny,
+    onRefresh,
+}) {
+    const itemRender = (item) => (
+        <>
+            <td>
+                <TableItem>{item.No}</TableItem>
+            </td>
+            <td>
+                <TableItem>{item.Code}</TableItem>
+            </td>
+            <td>
+                <TableItem>{item.Assetname}</TableItem>
+            </td>
+            <td>
+                <TableItem>{item.Assignedto}</TableItem>
+            </td>
+            <td>
+                <TableItem>{item.Assignedby}</TableItem>
+            </td>
+            <td>
+                <TableItem>{item.AssignedDate}</TableItem>
+            </td>
+            <td>
+                <TableItem>{item.Status}</TableItem>
+            </td>
+            <td className="table-actions">
+                <span className="table-icon" onClick={() => onEdit && onEdit(item)}>
+                    <BsPencil color="#dc3545" />
+                </span>
+                <span className="table-icon" onClick={() => onDeny && onDeny(item)}>
+                    <TiDeleteOutline className="border-0" />
+                </span>
+                <span className="table-icon" onClick={() => onRefresh && onRefresh(item)}>
+                    <TiRefresh className="border-0" />
+                </span>
+            </td>
+        </>
     );
+    return (
+        <NSTable
+            titles={tableTitles}
+            datas={datas}
+            totalPages={totalPage}
+            itemRender={itemRender}
+            onChangeSort={onChangeSort}
+            onChangePage={onChangePage}
+        />
+    )
+
 }
