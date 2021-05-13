94b714aeca84056db7982c937efd9d4bb536f6f0
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-05-13 18:03:22 +0700

    add userdetail model

---
+++ RookieOnlineAssetManagement/Models/UserDetailModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieOnlineAssetManagement.Models
+{
+    public class UserDetailModel :UserModel
+    {
+        public DateTime DateOfBirth { get; set; }
+        public bool Gender { get; set; }
+        public string Location { get; set; }
+    }
+}
