f1fde85ccc046530f10b5b35a50aee567ab23700
DangToan <dtoan2805@gmail.com>
2021-05-17 23:11:15 +0700

    Update Form Search

--- client-app/src/common/SearchBar.js
+++ client-app/src/common/SearchBar.js
@@ -1,19 +1,22 @@
 import React from "react";
+import { InputGroup, InputGroupAddon, Button, Input } from "reactstrap";
 
 export default function SearchBar({ onSearch }) {
-  function handleChange(e) {
-    console.log(e.target.query.value);
-    e.preventDefault();
-    onSearch && onSearch(e.target.query.value);
+  const queryInput = React.useRef();
+
+  function handleSubmit() {
+    onSearch && onSearch(queryInput.current.value);
   }
   return (
     <>
-      <form className="example" onSubmit={handleChange}>
-        <input type="text" name="query" />
-        <button type="submit">
-          <i className="fa fa-search" />
-        </button>
-      </form>
+      <InputGroup>
+        <Input innerRef={queryInput} />
+        <InputGroupAddon addonType="append">
+          <Button color="secondary" onClick={handleSubmit}>
+            <i className="fa fa-search" />
+          </Button>
+        </InputGroupAddon>
+      </InputGroup>
     </>
   );
 }
