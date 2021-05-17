3bdf737764a9cf9245661287e7445c9b4a478677
Mike0704 <haiduong7499@gmail.com>
2021-05-17 11:32:59 +0700

    search asset

--- client-app/src/common/SearchBar.js
+++ client-app/src/common/SearchBar.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-export default function SelectDate({ namedate }) {
+export default function SearchBar({ onSearch }) {
+  function handleChange(e) {
+    console.log(e.target.query.value);
+    e.preventDefault();
+    onSearch && onSearch(e.target.query.value);
+  }
   return (
     <>
-      <form className="example">
-        <input type="text" />
+      <form className="example" onSubmit={handleChange}>
+        <input type="text" name="query" />
         <button type="submit">
           <i className="fa fa-search" />
         </button>
