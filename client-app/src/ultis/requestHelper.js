d5873d7971780bbb187a05f75dd762ae337b7262
DangToan <dtoan2805@gmail.com>
2021-05-17 21:10:05 +0700

    Add Request Helper

---
+++ client-app/src/ultis/requestHelper.js
@@ -0,0 +1,10 @@
+export function _createQuery(params) {
+  if (!params) return "";
+  let queryStr = "";
+  for (const key in params) {
+    if (!params[key]) continue;
+    if (queryStr) queryStr += "&&";
+    queryStr += key + "=" + params[key];
+  }
+  return "?" + queryStr;
+}
