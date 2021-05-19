dae08e37ea7530c09da1dc511df911c1e9103f4a
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-05-19 11:57:12 +0700

    add model

---
+++ RookieOnlineAssetManagement/Models/AssetDetailModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieOnlineAssetManagement.Models
+{
+    public class AssetDetailModel : AssetModel
+    {
+        public string Specification { get; set; }
+        public DateTime InstalledDate { get; set; }
+        public string LocationName { get; set; }
+        public string LocationId { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
