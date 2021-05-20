fe429af659d817a42fb0b0947fcd1669252d7328
DangToan <dtoan2805@gmail.com>
2021-05-21 00:00:42 +0700

    Update User Table

---
+++ client-app/src/pages/user/UserFilterType.js
@@ -0,0 +1,23 @@
+import React from "react";
+import MultiSelect from "react-multi-select-component";
+import { userType } from "../../enums/userType";
+
+export default function UserFilterState({ onChange }) {
+  const [itemsSeleted, setItemsSeleted] = React.useState([]);
+
+  const handleSelected = (val) => {
+    setItemsSeleted(val);
+    onChange && onChange(val.map((item) => item.value));
+  };
+
+  return (
+    <MultiSelect
+      disableSearch
+      options={userType}
+      value={itemsSeleted}
+      onChange={handleSelected}
+      labelledBy="Select State"
+      ArrowRenderer={() => <span className="fa fa-filter" />}
+    />
+  );
+}
