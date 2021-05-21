8c90ed0749dfa42890fe8c1fcc7b9e491fde81e6
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-05-21 09:51:24 +0700

    update repository

--- RookieOnlineAssetManagement/Repositories/IAssignmentRepository.cs
+++ RookieOnlineAssetManagement/Repositories/IAssignmentRepository.cs
@@ -9,6 +9,7 @@ namespace RookieOnlineAssetManagement.Repositories
     {
         Task<AssignmentModel> CreateAssignmentAsync(AssignmentRequestModel assignmentRequestModel);
         Task<AssignmentModel> UpdateAssignmentAsync(string id, AssignmentRequestModel assignmentRequestModel);
+        Task<bool> ChangeStateAssignmentAsync(string id, StateAssignment state);
         Task<bool> DeleteAssignmentAsync(string id);
         Task<(ICollection<AssignmentModel> Datas, int TotalPage, int TotalItem)> GetListAssignmentAsync(StateAssignment[] StateAssignments, string AssignedDateAssignment, string query, SortBy AssetId, SortBy AssetName, SortBy AssignedTo, SortBy AssignedBy, SortBy AssignedDate, SortBy State, int page, int pageSize);
         Task<AssetDetailModel> GetAssignmentById(string id);
