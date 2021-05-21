8bc8038c781753cd77b80db14f2ad3f20e96dc28
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-05-21 14:17:51 +0700

    Add assignmentRequestParam Model

---
+++ RookieOnlineAssetManagement/Models/AssignmentRequestParams.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using RookieOnlineAssetManagement.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieOnlineAssetManagement.Models
+{
+    public class AssignmentRequestParams
+    {
+        
+        public StateAssignment[] StateAssignments { get; set; }
+        public string AssignedDateAssignment { get; set; }
+        public string LocationId { get; set; }
+        public string query { get; set; }
+        public SortBy? AssetId { get; set; }
+        public SortBy? AssetName { get; set; }
+        public SortBy? AssignedTo { get; set; }
+        public SortBy? AssignedBy { get; set; }
+        public SortBy? AssignedDate { get; set; }
+        public SortBy? State { get; set; }
+        public int page { get; set; }
+        public int pageSize { get; set; }
+    }
+}
