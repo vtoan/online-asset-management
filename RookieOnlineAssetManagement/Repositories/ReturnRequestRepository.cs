f4cec6d541965276a24ea989b4926e831e847c57
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-05-22 23:16:02 +0700

    add repository

---
+++ RookieOnlineAssetManagement/Repositories/ReturnRequestRepository.cs
@@ -0,0 +1,30 @@
+﻿using RookieOnlineAssetManagement.Data;
+using RookieOnlineAssetManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieOnlineAssetManagement.Repositories
+{
+    public class ReturnRequestRepository: IReturnRequestRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public ReturnRequestRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<ReturnRequestModel> CreateReturnRequestAsync(string assignmentId,string requestedUserId)
+        {
+            return null;
+        }
+        public async Task<bool> ChangeStateAsync(bool state, string assignmentId)
+        {
+            return true;
+        }
+        public async Task<ICollection<ReturnRequestModel>> GetListReturnRequestAsync(ReturnRequestParams returnRequestParams)
+        {
+            return null;
+        }
+    }
+}
