f82defb22bf3dd365a4b5bfcb1fdea9d217fcd5d
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-05-22 23:14:36 +0700

    add model

---
+++ RookieOnlineAssetManagement/Models/ReturnRequestModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieOnlineAssetManagement.Models
+{
+    public class ReturnRequestModel
+    {
+        public long AssignmentId { get; set; }
+        public string AssetId { get; set; }
+        public string AssetName { get; set; }
+        public string AcceptedUserId { get; set; }
+        public string RequestUserId { get; set; }
+        public string RequestBy { get; set; }
+        public string AcceptedBy { get; set; }
+        public DateTime? ReturnedDate { get; set; }
+        public bool State { get; set; }
+    }
+}
