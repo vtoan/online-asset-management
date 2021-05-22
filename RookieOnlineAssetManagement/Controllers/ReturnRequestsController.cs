bcd6cc2fda7478472453d42c8faf4453dc893afb
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-05-22 23:16:15 +0700

    add controller

---
+++ RookieOnlineAssetManagement/Controllers/ReturnRequestsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RookieOnlineAssetManagement.Models;
+using RookieOnlineAssetManagement.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieOnlineAssetManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReturnRequestsController : ControllerBase
+    {
+        private readonly IReturnRequestService _returnRequestService;
+        public ReturnRequestsController(IReturnRequestService returnRequestService)
+        {
+            _returnRequestService = returnRequestService;
+        }
+        [HttpPost]
+        public async Task<ActionResult<ReturnRequestModel>> CreateAsync(string assignmentId, string requestedUserId)
+        {
+            return Ok(await _returnRequestService.CreateReturnRequestAsync(assignmentId, requestedUserId));
+        }
+        [HttpPut]
+        public async Task<ActionResult<bool>> ChangeStateAsync(bool state, string assignmentId)
+        {
+            return Ok(await _returnRequestService.ChangeStateAsync(state, assignmentId));
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ReturnRequestModel>>> GetListAsync([FromQuery] ReturnRequestParams returnRequestParams)
+        {
+            return Ok(await _returnRequestService.GetListReturnRequestAsync(returnRequestParams));
+        }
+    }
+}
