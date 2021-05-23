72e83d5ef3c5b9baf8c9a218a6edd8619eb8fa94
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-05-23 17:54:04 +0700

    add service

---
+++ RookieOnlineAssetManagement/Services/IReportService.cs
@@ -0,0 +1,12 @@
+﻿using RookieOnlineAssetManagement.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RookieOnlineAssetManagement.Services
+{
+    public interface IReportService
+    {
+        Task<bool> ExportReportAsync(string locationId);
+        Task<ICollection<ReportModel>> GetListReportAsync(string locationId)
+    }
+}
