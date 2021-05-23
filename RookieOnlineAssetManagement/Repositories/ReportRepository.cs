cfe50120ab26c7b9f835fc9c48c6ae18a7930704
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-05-23 17:53:45 +0700

    add repository

---
+++ RookieOnlineAssetManagement/Repositories/ReportRepository.cs
@@ -0,0 +1,26 @@
+﻿using RookieOnlineAssetManagement.Data;
+using RookieOnlineAssetManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieOnlineAssetManagement.Repositories
+{
+    public class ReportRepository: IReportRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public ReportRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<bool> ExportReportAsync(string locationId)
+        {
+            return true;
+        }
+        public async Task<ICollection<ReportModel>> GetListReportAsync(string locationId)
+        {
+            return null;
+        }
+    }
+}
