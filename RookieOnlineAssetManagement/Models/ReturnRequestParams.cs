76470b1f403917c6b4d8ac1d5d4824efd8e739bd
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-05-23 14:29:05 +0700

    update model

--- RookieOnlineAssetManagement/Models/ReturnRequestParams.cs
+++ RookieOnlineAssetManagement/Models/ReturnRequestParams.cs
@@ -9,16 +9,16 @@ namespace RookieOnlineAssetManagement.Models
     public class ReturnRequestParams
     {
         public bool[] StateReturnRequests { get; set; }
-        public string ReturnedDateFilter { get; set; }
+        public string ReturnedDate { get; set; }
         public string LocationId { get; set; }
         public string query { get; set; }
-        public SortBy? AssetId { get; set; }
-        public SortBy? AssetName { get; set; }
-        public SortBy? RequestedBy { get; set; }
-        public SortBy? AcceptedBy { get; set; }
-        public SortBy? AssignedDate { get; set; }
-        public SortBy? ReturnedDate { get; set; }
-        public SortBy? State { get; set; }
+        public SortBy? sortAssetId { get; set; }
+        public SortBy? sortAssetName { get; set; }
+        public SortBy? sortRequestedBy { get; set; }
+        public SortBy? sortAcceptedBy { get; set; }
+        public SortBy? sortAssignedDate { get; set; }
+        public SortBy? sortReturnedDate { get; set; }
+        public SortBy? sortState { get; set; }
         public int page { get; set; }
         public int pageSize { get; set; }
     }
