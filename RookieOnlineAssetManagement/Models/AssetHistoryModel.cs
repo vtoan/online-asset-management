d686a2566b806afee426f1132fd2e9b5eb65ee35
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-05-23 15:44:22 +0700

    add model

---
+++ RookieOnlineAssetManagement/Models/AssetHistoryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieOnlineAssetManagement.Models
+{
+    public class AssetHistoryModel
+    {
+        public string AssignmentId { get; set; }
+        public DateTime Date { get; set; }
+        public string  AssignedTo { get; set; }
+        public string AssignedBy { get; set; }
+        public DateTime ReturnedDate { get; set; }
+    }
+}
