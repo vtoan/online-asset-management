6854b024afe1500f20eac2dc9598b539b91289a3
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-05-24 11:50:28 +0700

    add model

---
+++ RookieOnlineAssetManagement/Models/AssetRequestParams.cs
@@ -0,0 +1,22 @@
+﻿using RookieOnlineAssetManagement.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieOnlineAssetManagement.Models
+{
+    public class AssetRequestParams
+    {
+        public StateAsset[] State { get; set; }
+        public string[] CategoryId { get; set; }
+        public string Query { get; set; }
+        public SortBy? SortCode { get; set; }
+        public SortBy? SortName { get; set; }
+        public SortBy? SortCate { get; set; }
+        public SortBy? SortState{ get; set; } 
+        public string LocationId { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+    }
+}
