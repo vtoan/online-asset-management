0ed7ef35abf77361d2bc179586a43e827e39badf
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-05-28 17:38:18 +0700

    Add Asset Service Test

--- RookieOnlineAssetManagement.UnitTests/Service/AssetServiceTest.cs
+++ RookieOnlineAssetManagement.UnitTests/Service/AssetServiceTest.cs
@@ -84,6 +84,14 @@ namespace RookieOnlineAssetManagement.UnitTests.Service
         public async Task GetListHistory_Success()
         {
             var mockAssetRepo = new Mock<IAssetRepository>();
+            string assetid = Guid.NewGuid().ToString();
+            AssetHistoryModel Model = new AssetHistoryModel();
+            List<AssetHistoryModel> collection = new List<AssetHistoryModel>();
+            mockAssetRepo.Setup(x => x.GetListAssetHistoryAsync(It.IsAny<string>())).ReturnsAsync(collection);
+            var request = new AssetService(mockAssetRepo.Object);
+            var result = await request.GetListAssetHistoryAsync(assetid);
+            Assert.NotNull(result);
+
         }
             
     }
