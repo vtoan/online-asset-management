f4a525bebf9f019cd6bc929d4486d05199fa0014
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-06-03 17:35:24 +0700

    update controller

--- RookieOnlineAssetManagement/Controllers/ReportsController.cs
+++ RookieOnlineAssetManagement/Controllers/ReportsController.cs
@@ -28,44 +28,7 @@ namespace RookieOnlineAssetManagement.Controllers
         {
             reportRequestParams.LocationId = RequestHelper.GetLocationSession(HttpContext);
             var report = await _reportService.ExportReportAsync(reportRequestParams);
-            using (var workbook = new XLWorkbook())
-            {
-                var worksheet = workbook.Worksheets.Add("Report Category");
-                var currentRow = 1;
-                worksheet.Cell(currentRow, 1).Style.Font.SetBold();
-                worksheet.Cell(currentRow, 2).Style.Font.SetBold();
-                worksheet.Cell(currentRow, 3).Style.Font.SetBold();
-                worksheet.Cell(currentRow, 4).Style.Font.SetBold();
-                worksheet.Cell(currentRow, 5).Style.Font.SetBold();
-                worksheet.Cell(currentRow, 6).Style.Font.SetBold();
-                worksheet.Cell(currentRow, 7).Style.Font.SetBold();
-                worksheet.Cell(currentRow, 1).Value = "Category";
-                worksheet.Cell(currentRow, 2).Value = "Total";
-                worksheet.Cell(currentRow, 3).Value = "AssignedTotal";
-                worksheet.Cell(currentRow, 4).Value = "AvailableTotal";
-                worksheet.Cell(currentRow, 5).Value = "NotAvailableTotal";
-                worksheet.Cell(currentRow, 6).Value = "WatingRecyclingTotal";
-                worksheet.Cell(currentRow, 7).Value = "RecycledTotal";
-                foreach (var item in report)
-                {
-                    currentRow++;
-                    worksheet.Cell(currentRow, 1).Value = item.CategoryName;
-                    worksheet.Cell(currentRow, 2).Value = item.Total;
-                    worksheet.Cell(currentRow, 3).Value = item.AssignedTotal;
-                    worksheet.Cell(currentRow, 4).Value = item.AvailableTotal;
-                    worksheet.Cell(currentRow, 5).Value = item.NotAvailableTotal;
-                    worksheet.Cell(currentRow, 6).Value = item.WatingRecyclingTotal;
-                    worksheet.Cell(currentRow, 7).Value = item.RecycledTotal;
-                }
-
-                using (var stream = new MemoryStream())
-                {
-                    workbook.SaveAs(stream);
-                    var content = stream.ToArray();
-
-                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Reports.xlsx");
-                }
-            }
+            return ExportFileHelper.ExportExcel(report, "Report Category", "Reports");
         }
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ReportModel>>> GetListAsync([FromQuery]ReportRequestParams reportParams)
