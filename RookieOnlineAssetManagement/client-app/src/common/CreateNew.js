b2513eea749d492b4ca5d7081f02505cdbec1fb7
DangToan <dtoan2805@gmail.com>
2021-06-03 18:17:35 +0700

    Update Create New Component

--- RookieOnlineAssetManagement/client-app/src/common/CreateNew.js
+++ RookieOnlineAssetManagement/client-app/src/common/CreateNew.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Button } from "reactstrap";
 
-export default function CreateNew({ namecreate }) {
+export default function CreateNew({ namecreate, onClick }) {
   return (
-    <Button className="btn-create" color="danger">
+    <Button
+      className="btn-create"
+      color="danger"
+      onClick={(e) => onClick && onClick(e)}
+    >
       {namecreate}
     </Button>
   );
