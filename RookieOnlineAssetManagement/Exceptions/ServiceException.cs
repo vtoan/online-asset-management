76d6070c25beed923a765ed075df14ce8b3e96c3
LePhamThuyDuyen <59944850+LePhamThuyDuyen@users.noreply.github.com>
2021-06-04 14:54:37 +0700

    Add Service Exception

---
+++ RookieOnlineAssetManagement/Exceptions/ServiceException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieOnlineAssetManagement.Exceptions
+{
+    public class ServiceException : Exception
+    {
+        public ServiceException() { }
+        DayOfWeek dayofweek;
+        public Exception AgeNoValidException()
+        {
+            throw new Exception("Age is not valid");
+        }
+        public Exception JoinDateException()
+        {           
+            throw new Exception("Joined Date is : " + dayofweek.ToString());
+        }
+        public Exception CompareDateException()
+        {
+            throw new Exception("Joined Date is smaller tham Birth Of Date");
+        }
+        public Exception ReturnDateNotValidException()
+        {
+            throw new Exception("Return date not valid !");
+        }
+        public Exception PageException()
+        {
+            throw new Exception("Page and Page size not valid !");
+        }
+        public Exception CompareDateAssignedException()
+        {
+            throw new Exception("Assigned Date is smaller than Today");
+        }
+
+    }
+}
